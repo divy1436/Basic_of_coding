b86c2db398fbcbc843e0ead8544c7a9e541251d2
2024pceacsdivyanshu58@poornima.org <2024pceacsdivyanshu58@poornima.org>
2024-11-23 02:25:20 +0530

    python basics
    chapter 0,1 and their practice

---
+++ python/ch1/02_modules.py
@@ -0,0 +1,8 @@
+import pyjokes
+
+# print("Printing jokes>..") #use for commenting a line 
+# short cut for this(ctrl+/)
+
+joke=pyjokes.get_jokes()
+print(joke)
+
---
+++ python/ch1/04_pp3.py
@@ -0,0 +1,5 @@
+#install pyttsx3 used for reading text
+import pyttsx3
+engine=pyttsx3.init()
+engine.say("Hey I am good u fuck off")
+engine.runAndWait()

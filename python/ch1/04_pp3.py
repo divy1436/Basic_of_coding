c6e352b2f5d01a91a4ecf1458bb4b717e3f7e78b
2024pceacsdivyanshu58@poornima.org <2024pceacsdivyanshu58@poornima.org>
2024-11-26 01:17:50 +0530

    html

--- python/ch1/04_pp3.py
+++ python/ch1/04_pp3.py
@@ -1,5 +1,5 @@
 #install pyttsx3 used for reading text
 import pyttsx3
 engine=pyttsx3.init()
-engine.say("arya is very very good girl so now fuck off")
+engine.say("harsh fuck you")
 engine.runAndWait()

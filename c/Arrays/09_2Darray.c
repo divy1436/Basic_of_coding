18148d8ab3f1806b0719db969a316df1ea57efc9
2024pceacsdivyanshu58@poornima.org <2024pceacsdivyanshu58@poornima.org>
2025-01-22 02:14:56 +0530

    array

--- c/Arrays/09_2Darray.c
+++ c/Arrays/09_2Darray.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main(){
+    // Initialize a 2D array
     int arr[2][3]={{1,2,3},{4,5,6}};
+    // Display array elements
     printf("2d array elements:\n");
     for(int i=0;i<2;i++){
         for(int j=0;j<3;j++){

86cb50c1386bf017468b5ce3cc78190bf80e5717
2024pceacsdivyanshu58@poornima.org <2024pceacsdivyanshu58@poornima.org>
2025-03-17 22:56:23 +0530

    c++

--- c++/oops/01_class.cpp
+++ c++/oops/01_class.cpp
@@ -2,28 +2,45 @@
 #include <string>
 using namespace std;
 
-class Teacher{
+class Teacher {
+private:
+    double salary; // hiding data
 
-    private:
-    double salary;
-
-    public:
+public:
     string name;
     string dept;
     string subject;
-    
-    void changeDept (string newDept){
-        dept=newDept;
+
+    // Constructor
+    Teacher() {
+        dept = "CSE";
     }
 
-};
-int main(){
-    Teacher t1;
-    t1.name="divyanshu";
-    t1.dept="CSE";
-    t1.subject="Maths";
-    t1.salary=500; 
-    cout <<t1.name <<endl;
-    return 0;
+    // Setter for salary
+    void setSalary(double s) {
+        salary = s;
+    }
 
+    // Getter for salary
+    double getSalary() {
+        return salary;
+    }
+
+    void changeDept(string newDept) {
+        dept = newDept;
+    }
+};
+
+int main() {
+    Teacher t1; // constructor call
+    t1.name = "Divyanshu";
+    t1.subject = "Maths";
+    t1.setSalary(500); // Set salary using setter
+
+    cout << "Name: " << t1.name << endl;
+    cout << "Department: " << t1.dept << endl;
+    cout << "Subject: " << t1.subject << endl;
+    cout << "Salary: " << t1.getSalary() << endl;
+
+    return 0;
 }

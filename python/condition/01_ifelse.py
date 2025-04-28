a85ebd63089374d9322cfa68a4c6ba42dd270f24
Divyanshu Maurya <2024pceacsdivyanshu58@poornima.org>
2025-04-28 23:28:58 +0530

    ifelse

--- python/condition/01_ifelse.py
+++ python/condition/01_ifelse.py
@@ -1,23 +1,22 @@
+# Check if the person is eligible to vote based on age
 age = 16
 if(age >= 18):
-    print(
-        "can vote"
-    )
+    print("can vote")  # If age is 18 or older, they can vote
 else:
-    print("can not drive")
-    
+    print("can not drive")  # Otherwise, print "can not drive" (though it would be clearer to say "cannot vote")
 
-light ="green"
+# Check traffic light color and give instruction
+light = "green"
 if(light == "red"):
-    print("stop")
+    print("stop")  # If the light is red, instruct to stop
 elif(light == "green"):
-    print("go") 
+    print("go")  # If the light is green, instruct to go
 elif(light == "yellow"):
-    print("wait") 
+    print("wait")  # If the light is yellow, instruct to wait
 
+# Check number and give a message
 num = 3  
-if(num <5):
-    print("the number is greater")
-if(num <1):
-    print("the num is not greter than ")    
-
+if(num < 5):
+    print("the number is greater")  # (Actually, the message is confusing — number is **less** than 5)
+if(num < 1):
+    print("the num is not greater than")  # Will not print because 3 is not less than 1

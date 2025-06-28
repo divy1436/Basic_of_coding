3166af4bb2fe97db05971c30bdd96e9f3f83e7aa
Divyanshu Maurya <2024pceacsdivyanshu58@poornima.org>
2025-06-28 23:50:16 +0530

    Update firstprogram.java

--- java/chapter1/firstprogram.java
+++ java/chapter1/firstprogram.java
@@ -3,7 +3,7 @@ public class firstprogram {
     public static void main(String[] args) {
         int a =3;
         int b =6;
-        int sum = a+b+a;
+        int sum = a+b;
         System.out.println("The sum of a and b");
         System.out.println(sum);
     }

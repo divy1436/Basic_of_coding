e1631052c143362494df768dccf1b11ea995203a
Divyanshu Maurya <2024pceacsdivyanshu58@poornima.org>
2025-07-09 20:42:09 +0530

    d

--- java/chapter2/c7input.java
+++ java/chapter2/c7input.java
@@ -2,13 +2,13 @@ import java.util.Scanner;
 
 public class c7input {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter the radius");
         int radius;
         radius = sc.nextInt();
-        double pi=3.14;
-        double area = pi*radius*radius;
-        System.out.println("the area of circle is" +area);
+        double pi = 3.14;
+        double area = pi * radius * radius;
+        System.out.println("the area of circle is" + area);
 
 
     }

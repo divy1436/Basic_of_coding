b6222bfe789e7829267326bd4ef5461faec4457f
Divyanshu Maurya <2024pceacsdivyanshu58@poornima.org>
2025-07-28 17:37:24 +0530

    bv

---
+++ src/com/divy/Comdtion.java
@@ -0,0 +1,15 @@
+package com.divy;
+
+public class Comdtion {
+    public static void main(String[] args) {
+        int salary=5000;
+        if(salary>5000){
+            salary+=1000;
+
+        }
+        else {
+           salary+=500;
+        }
+        System.out.println("Salary is: "+salary);
+    }
+}
